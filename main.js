f4780f57753ee9ad29bcc8e044e99c9540be2518
Bozo2023 <ulrob1.0@email.com>
2024-02-06 22:54:53 +0100

    exactement

--- main.js
+++ main.js
@@ -4,41 +4,23 @@ const { createApp }=Vue
 createApp({
   data(){
       return{
-        mailList : []
+                 mailList : []
        
-      } 
-  },
+            } 
+        },
 
-  mounted(){
+         mounted(){
+                    for(let i=0; i< 10 ; i++){
+                    axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (response ) => {
+                    console.log (response.data)
 
-  },
+                    this.mailList.push(response.data.response)
+                } )
+            }
+
+        },
   methods:{
-    generaMailList(){
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then( (result ) => {
-            console.log (result.data.response)
-            this.mailList=result.data.response
-
-        })
-        for (let i = 0; i < 10 ; i++){
-            generaMailList ().then( email =>{
-                mailList.push(email);
-
-                if(email.length === 10){
-                    let list = document.getElementById ('list');
-                    mailList.forEach(email => {
-                        let item = document.createElement('li');
-                        item.textContent = email;
-                        mailList.appendChild(item);
-                        
-                    });
-                    list.appendChild(item);
-                }
-                
-            });
-        }
-    }
-  
-      
-
-  }
-}).mount('#app')
+   
+          }
+            
+},).mount('#app')
